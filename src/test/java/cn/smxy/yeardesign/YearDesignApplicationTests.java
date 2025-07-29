dc77966dfddfbf28099004f321e1a30e429d004b
sgy1205 <2174892027@qq.com>
2025-07-29 15:59:43 +0800

    first commit

---
+++ src/test/java/cn/smxy/yeardesign/YearDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.smxy.yeardesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YearDesignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
